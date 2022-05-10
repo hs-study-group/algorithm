e6478f3da4d6e17a05504e05220b9a4a83336270
Rae won, Jeong <83106564+Blanc-et-noir@users.noreply.github.com>
2022-05-10 22:14:40 +0900

    [BOJ] 가장 긴 증가하는 부분 수열 3 / Gold2

--- BOJ/DP/12738/Blanc_et_Noir/Main.java
+++ BOJ/DP/12738/Blanc_et_Noir/Main.java
@@ -9,13 +9,17 @@ public class Main {
 	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	
+	//lowerbound 값의 인덱스를 반환하는 메소드
+	//lowerbound란 자신보다 큰 값들중 가장 작은값을 의미
 	public static int getLowerBound(int[] arr,int val, int s, int e) {
 		int m = (s+e)/2;
 		if(s==e) {
 			return m;
 		}
+		//자기자신도 lowerbound가 될 수 있으므로 자기자신도 포함하여 탐색
 		if(arr[m]>=val) {
 			return getLowerBound(arr,val,s,m);
+		//자기자신보다 작은 값들은 lowerbound가 될 수 없음
 		}else {
 			return getLowerBound(arr,val,m+1,e);
 		}
@@ -36,8 +40,11 @@ public class Main {
 		result[0] = arr[0];
 		
 		for(int i=0; i<arr.length; i++) {
+			//입력값 <= 임시배열의 마지막 값 이면
+			//더 길이가 길어질 가능성이 있도록 값을 교체
 			if(arr[i]<=result[cnt]) {
 				result[getLowerBound(result,arr[i],0,cnt)] = arr[i];
+			//더 길어질 가능성이 없으므로 그냥 추가만 함
 			}else{
 				result[++cnt] = arr[i];
 			}
