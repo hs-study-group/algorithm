a3fc441eda54d5d19c7f310133ab5384da518d4d
calisyj <ihyj88@naver.com>
2022-05-26 02:46:39 +0900

    [PGS] 두 개 뽑아서 더하기 / Level1

---
+++ PGS/Sort/두 개 뽑아서 더하기/calisyj/두 개 뽑아서 더하기.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = []
+    
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i != j :
+                answer.append(numbers[i] + numbers[j])
+    return sorted(list(set(answer)))
